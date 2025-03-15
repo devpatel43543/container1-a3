a001a0a0b44046e7379b048557243a438dafcdc1
Library User <lib-user@T9346.WPA.Dal.Ca>
2025-03-15 15:35:44 -0300

    added log

--- container1/index.js
+++ container1/index.js
@@ -7,7 +7,7 @@ dotenv.config(); // Load .env file
 const app = express();
 const PORT = process.env.PORT || 6000;
 
-app.use(express.json()); // Use built-in express JSON parser
+app.use(express.json()); // Use built-in express JSON parser  
 
 app.use("/", fileRoutes);
 
